a5aee6b68f95d6115a5dd4146f2febae83980c3f
MichaelSch91 <michael_s33@gmx.de>
2025-02-10 13:51:49 +0100

    Kommentare

--- Default32squareroot.h
+++ Default32squareroot.h
@@ -9,14 +9,18 @@
 
 class Default32squareroot {
 public:
+	// Konstruktoren
+	// 
+	// Standardkonstruktor, mit ba als Basis -> sqrt(ba), s, e, m mit 0 initialisiert
 	Default32squareroot(int ba);
-
+	// Konstruktor, mit ba als Basis -> sqrt(ba), s, e, m gem. Aufruf initialisiert
 	Default32squareroot(int ba, int s, int e, int m);
-
+	// Konstruktor mit Basis 2 -> sqrt(2), s, e, m gem. Aufruf initialisiert
 	Default32squareroot(int s, int e, int m);
 
 	void setValues(int s, int e, int m);
 
+	// Berechnung des dargestellten Wertes
 	long double calcX();
 	long double calcX_highExponent();
 	long double calcX_oddExponent();
@@ -25,52 +29,68 @@ public:
 	long double calc_pow_oddExponent();
 	unsigned long long int calc_pow_evenExponent();
 	long double calc_pow_negativeExponent();
-
+	// Berechnung des dargestellten Wertes ohne sqrt(ba)^e umzuformen, für Tests notwendig
+	// lässt die Größenordnung des Ergebnisses mit calcX() vergleichen und zeigt, wie viel 
+	// genauer die Berechnung mit Umformen von sqrt(ba)^e ist
 	long double simpleCalcX();
+	// Redundant zu calcX(), aber wird in manchen Funktionen noch aufgerufen, da es in anderen Klassen nötig war
 	long double calcX_long_double();
-
+	// nur für Tests notwendig
+	double berechneMantisseDezimalwert() const;
 	// gibt den Wert der Abweichung zurück, um den ein Mantissenbit (m+1) den Wert abhängig vom Exponenten steigert
-	// Delta = Zahl(0,e,1) - Zahl(0,e,0)
+	// Delta = Zahl(0,this->e,1) - Zahl(0,this->e,0)
 	long double deviation_due_to_exp();
 
+	// Operatoren
+	// 
+	// überladene Operatoren
 	Default32squareroot operator+(Default32squareroot a);
-	Default32squareroot plus_different_operator(Default32squareroot a);
-
 	Default32squareroot operator-(Default32squareroot a);
+	Default32squareroot operator*(Default32squareroot a);
+	bool operator==(Default32squareroot a);
+	// Fälle, in denen es sinnvoller ist eine andere Methode aufzurufen (zB a.operator+(-b) soll a.operator-(+b) aufrufen)
+	Default32squareroot plus_different_operator(Default32squareroot a);
 	Default32squareroot minus_different_operator(Default32squareroot a);
 
-	Default32squareroot operator*(Default32squareroot a);
-
-	bool operator==(Default32squareroot a);
-
+	// Getter und Setter
+	// 
+	// Rückgabedatentypen entsprechen nicht zwingend den Datentypen der Klassen-Attribute,
+	// da der Compiler sonst, zB bei der Mantisse, (int * long long) = int interpretiert und so 
+	// Überläufe erzeugt
 	int getSign();
 	void setSign(int s);
-
 	unsigned long long getMantissa();
 	void setMantissa(int m);
-
 	int getExponent();
 	void setExponent(int e);
-
+	// nur Getter, da nicht mehr veränderbar
 	long long getBase();
 	int getBias();
 	int getMantissa_bits();
 	int getExponent_bits();
-
+	// Min und Max Werte für Exponent und Mantisse
 	int getExponent_max();
 	int getExponent_min();
 	unsigned long long getMantissa_max();
 	unsigned long long getMantissa_min();
 
-	// richtige Tests erstellen, zumindest für die Methoden der entwickelten Zahlendarstellung
+	// Tests
+	// 
+	// ggf in eigene Testklasse packen (todo)
+	//
+	// Operatoren
 	void test_Default32squareroot_operator_plus();
 	void test_Default32squareroot_operator_minus();
 	void test_Default32squareroot_operator_multiply();
-	
+	// Tests für die Berechnung des dargestellten Werts
 	void test_Default32squareroot_calcX();
+	// Tests für den Converter von double (oder long double) Darstellung zur Zahlendarstellung der Klasse
 	void test_Default32squareroot_convert_to_Default32squareroot();
 	void test_Default32squareroot_convert_to_Default32squareroot_In_and_Out(); // Default32squareroot als Random-Eingabe und Überprüfung, ob ein Default32squareroot-Objekt mit gleichen Wert raus kommt.
 
+	// Converter
+	//
+	// double / long double wird in Default32Squareroot Zahlentyp konvertiert
 	static Default32squareroot convert_to_Default32squareroot(int base, long double x);
 	void convert_setSign(long double x);
 	int convert_findExponent(long double x);
@@ -86,13 +106,10 @@ private:
 	static constexpr int bias = 127;  // Biaswert für Exponent
 	static constexpr int mantissa_bits = 23;  // Bits Mantisse
 	static constexpr int exponent_bits = 8;  // Bits Mantisse
-	int exponent_max = 255;
-	int exponent_min = 0;
-	unsigned long long mantissa_max = 8'388'607;
-	unsigned long long mantissa_min = 0;
-
-
-	double berechneMantisseDezimalwert() const;
+	int exponent_max = 255; // max Wert Exponent
+	int exponent_min = 0; // min Wert Exponent
+	unsigned long long mantissa_max = 8'388'607; // max Wert Mantisse
+	unsigned long long mantissa_min = 0; // min Wert Mantisse
 };
 
 #endif
