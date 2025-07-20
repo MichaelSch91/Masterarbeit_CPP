d34913b787141130c8ccf1508cd09bbff7760849
MichaelSch91 <michael_s33@gmx.de>
2025-07-20 18:18:10 +0200

    zeitmessung

--- LogisticMap.cpp
+++ LogisticMap.cpp
@@ -32,72 +32,61 @@
 //config
 bool create_csv = 1;
 
-static void logMap_default32squareroot();
-static void logMap_float();
+static void logMap_default32squareroot(double s, double start_increment, double r);
+static void logMap_float(double s, double start_increment, double r);
 
 
-static void zeitmessung_logmap_default32squareroot() {
+static void zeitmessung_logmap_default32squareroot(double s, double start_increment, double r) {
 	auto start = std::chrono::high_resolution_clock::now();
-	logMap_default32squareroot();
+	logMap_default32squareroot(s, start_increment, r);
 	auto ende = std::chrono::high_resolution_clock::now();
 
 	auto dauer = std::chrono::duration_cast<std::chrono::milliseconds>(ende -
 		start);
-	std::cout << "Die Simulation der logistischen Abbildung mit Default32squareroot dauerte: " << dauer.count() << " Millisekunden." << std::endl;
+	std::cout << "Default32Squareroot: r = " << r << " s = " << s << " s_incr = " << start_increment << std::endl;
+	std::cout << "Die Simulation der logistischen Abbildung dauerte: " << dauer.count() << " Millisekunden." << std::endl;
 }
 
-static void zeitmessung_logmap_float() {
+static void zeitmessung_logmap_float(double s, double start_increment, double r) {
 	auto start = std::chrono::high_resolution_clock::now();
-	logMap_float();
+	logMap_float(s, start_increment, r);
 	auto ende = std::chrono::high_resolution_clock::now();
 
 	auto dauer = std::chrono::duration_cast<std::chrono::milliseconds>(ende -
 		start);
-	std::cout << "Die Simulation der logistischen Abbildung mit Float dauerte: " << dauer.count() << " Millisekunden." << std::endl;
+	std::cout << "Float: r = " << r << " s = " << s << " s_incr = " << start_increment << std::endl;
+	std::cout << "Die Simulation der logistischen Abbildung dauerte: " << dauer.count() << " Millisekunden." << std::endl;
 }
 
-static void logMap_float() {
-	double START = 0.01;
-	double START_INCREMENT = 0.01;
-	double R_INCREMENT = 0.01;
+static void logMap_float(double s, double start_increment, double r) {
+	double START = s;
+	double START_INCREMENT = start_increment;
 
 	double start = START;
-	double r = 4.00;
 
-	while (r <= 4.00) {
-		while (start < 1.0) {
-			Logisticmap_float fl(start, r);
-			std::cout << "Float, ";
-			// fl.list_screen_csv(fl.get_long_double_List());
-			start += START_INCREMENT;
-		}
-		start = START;
-		r += R_INCREMENT;
-		std::cout << '\n' << '\n';
+	while (start < 1.0) {
+		Logisticmap_float fl(start, r);
+		// std::cout << "Float, ";
+		// fl.list_screen_csv(fl.get_long_double_List());
+		start += START_INCREMENT;
 	}
+
+	std::cout << '\n' << '\n';
 }
 
-static void logMap_default32squareroot() {
-	double START = 0.01;
-	double START_INCREMENT = 0.01;
-	double R_INCREMENT = 0.01;
+static void logMap_default32squareroot(double s, double start_increment, double r) {
+	double START = s;
+	double START_INCREMENT = start_increment;
 
 	double start = START;
-	double r = 4.00;
 
-	while (r <= 4.00) {
-		while (start < 1.0) {
-
-			Logisticmap_Default32squareroot logMap(Default32squareroot::convert_to_Default32squareroot(2, start), Default32squareroot::convert_to_Default32squareroot(2, r));
-			std::cout << "squareroot, ";
-			// logMap.list_screen_csv();
-			start += START_INCREMENT;
-
-		}
-		start = START;
-		r += R_INCREMENT;
-		std::cout << '\n' << '\n';
+	while (start < 1.0) {
+		Logisticmap_Default32squareroot logMap(Default32squareroot::convert_to_Default32squareroot(2, start), Default32squareroot::convert_to_Default32squareroot(2, r));
+		// std::cout << "squareroot, ";
+		// logMap.list_screen_csv();
+		start += START_INCREMENT;
 	}
+	std::cout << '\n' << '\n';
 }
 
 
@@ -105,14 +94,31 @@ int main()
 {
 	std::cout << "Start" << '\n';
 
+	double start = 0.01;
+	double start_increment = 0.01;
+	double r = 4.0;
+
+	int iterations = 50;
+
+	for (int i = 0; i < iterations; i++) {
+		while (r <= 4) {
+			zeitmessung_logmap_default32squareroot(start, start_increment, r);
+
+			zeitmessung_logmap_float(start, start_increment, r);
+		}
+	}
+
+
+
+	/*
 	double START = 0.01;
 	double START_INCREMENT = 0.01;
-	double R_INCREMENT = 0.01;
+	double R_INCREMENT = 0.002;
 
 	double start = START;
-	double r = 3.80;
+	double r = 3.81;
 
-	while (r <= 4.00) {
+	while (r <= 3.868) {
 		while (start < 1.0) {
 
 			Logisticmap_float fl(start, r);
@@ -129,7 +135,7 @@ int main()
 		r += R_INCREMENT;
 		std::cout << '\n' << '\n';
 	}
-
+	*/
 
 	std::cout << '\n' << "End" << '\n';
 }
